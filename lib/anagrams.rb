e9f2d7eacb4a2c5fcde379f8867c86f032b08fc3
Anatoliy Klots <klots@uw.edu>
2017-04-21 11:59:01 -0700

    add initial word check, antigram check, anagram and palindrome checks

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,24 +1,36 @@
 
 
-test1 = "race fast safe car"
-test2 = "raceFastsafecar  ."
-back_to_string1 =  []
 
-vowel_array = ["a", "i", "e", "o", "u", "y"]
+test1 = "zzza"
+test2 = "bob  ."
 
 input1_formated =  test1.downcase.gsub(/[^a-z0-9]/i, '')
+input1_array = input1_formated.split("")
 input2_formated =  test2.downcase.gsub(/[^a-z0-9]/i, '')
-input1_reversed = input1_formated.reverse!
-if input1_formated.size != input2_formated.size
-  puts "Not an anagram!"
-else
-  if input1_formated.chars.sort == input2_formated.chars.sort
-    if input2_formated == input1_reversed
-    puts "But it's also a palindrome!"
-  else
-    puts "Indeed"
-  end
-else
-  puts "alas"
-end
+input2_array = input2_formated.split("")
+vowel_array = ["a", "i", "e", "o", "u", "y"]
+  input1_reversed = input1_formated.reverse!
+
+
+
+#1 real word check
+if (input1_array & vowel_array).any? && (input2_array & vowel_array).any?
+#2 anything in common check
+  if (input1_array & input2_array).any?
+    #3 two inputs characters are equal check
+      if input1_formated.chars.sort == input2_formated.chars.sort
+       #4 palindrom check
+        if input2_formated == input1_reversed
+          puts "But these are also palindromes!"
+        else #4 end
+          puts "These are anagrams"
+        end
+      else #3 end
+        puts "These are not anagrams"
+      end
+    else  #2 end
+      puts "These words have no letter matches and are antigrams."
+    end
+  else #1 end
+    puts "enter a real word"
 end
